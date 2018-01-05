b9d27e79098f31147fad30b83141f0adf10e0e65
pcmaffey <peter@astronomer.io>
2018-01-05 10:53:01 -0700

    Fix undefined cookie error

--- js/keen.js
+++ js/keen.js
@@ -3,7 +3,8 @@
     projectId: "598a6e5ec9e77c00011811fa",
     readKey: "4E25C1A17EC7BFBE2FA058108F133D23C59768E748F397AB15224DFF7E8391A76519FBB4CEF8FA2ADDD7765AEFC276462E512EBB2D06201223030B52C7BF6B8DB42C687AD2C2402B6119957256D72E521C5C02DC7E92F1DBAFCC745258903361"
   });
-  var anonymousId = Cookies.get('ajs_anonymous_id').replace(/\"/g, "", true)
+  const cookie = Cookies.get('ajs_anonymous_id');
+  var anonymousId = cookie ? cookie.replace(/\"/g, "", true) : null;
 
   var chart_left = new Keen.Dataviz()
   .el("#keen-left")
